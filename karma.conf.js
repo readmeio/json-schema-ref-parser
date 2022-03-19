d93e336dbaee4eaf6f57103c6898f06a2010f0e2
Jon Ursenbach <jon@ursenba.ch>
2022-03-18 18:21:45 -0700

    chore: moving tests over to our code standards

--- karma.conf.js
+++ karma.conf.js
@@ -2,14 +2,12 @@
 // https://karma-runner.github.io/0.12/config/configuration-file.html
 // https://jstools.dev/karma-config/
 
-"use strict";
-
-const { karmaConfig } = require("@jsdevtools/karma-config");
-const { host } = require("@jsdevtools/host-environment");
+const { karmaConfig } = require('@jsdevtools/karma-config');
+const { host } = require('@jsdevtools/host-environment');
 
 module.exports = karmaConfig({
-  sourceDir: "lib",
-  fixtures: "test/fixtures/**/*.js",
+  sourceDir: 'lib',
+  fixtures: 'test/fixtures/**/*.js',
   browsers: {
     chrome: true,
     firefox: true,
